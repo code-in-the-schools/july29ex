37bf96e0278636f74dd0fcee354d6e519d12ad9d
TyTy <Tylieafter1@gmail.com>
2020-07-29 17:17:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,132 @@
+
+  function setup() {
+  createCanvas(710, 400, WEBGL);
+  snowflake();
+  stroke(255);
+
+let cx, cy;
+let secondsRadius;
+let minutesRadius;
+let hoursRadius;
+let clockDiameter;
+
+function setup() {
+  createCanvas(720, 400);
+  stroke(255);
+
+  let radius = min(width, height) / 2;
+  secondsRadius = radius * 0.71;
+  minutesRadius = radius * 0.6;
+  hoursRadius = radius * 0.5;
+  clockDiameter = radius * 1.7;
+
+  cx = width / 2;
+  cy = height / 2;
+}
+
+function draw() {
+  background(230);
+
+  // Draw the clock background
+  noStroke();
+  fill(244, 122, 158);
+  ellipse(cx, cy, clockDiameter + 25, clockDiameter + 25);
+  fill(237, 34, 93);
+  ellipse(cx, cy, clockDiameter, clockDiameter);
+
+  // Angles for sin() and cos() start at 3 o'clock;
+  // subtract HALF_PI to make them start at the top
+  let s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
+  let m = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
+  let h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
+
+  // Draw the hands of the clock
+  stroke(255);
+  strokeWeight(1);
+  line(cx, cy, cx + cos(s) * secondsRadius, cy + sin(s) * secondsRadius);
+  strokeWeight(2);
+  line(cx, cy, cx + cos(m) * minutesRadius, cy + sin(m) * minutesRadius);
+  strokeWeight(4);
+  line(cx, cy, cx + cos(h) * hoursRadius, cy + sin(h) * hoursRadius);
+
+  // Draw the minute ticks
+  strokeWeight(2);
+  beginShape(POINTS);
+  for (let a = 0; a < 360; a += 6) {
+    let angle = radians(a);
+    let x = cx + cos(angle) * secondsRadius;
+    let y = cy + sin(angle) * secondsRadius;
+    vertex(x, y);
+  }
+  endShape();
+}
+
+
+  
+ 
+function drawScene() {
+   background('brown');
+
+  let locX = mouseX - height / 2;
+  let locY = mouseY - width / 2;
+
+  ambientLight(50);
+  directionalLight(255, 0, 0, 0.25, 0.25, 0);
+  pointLight(0, 0, 255, locX, locY, 250);
+
+  push();
+  translate(-width / 4, 0, 0);
+  rotateZ(frameCount * 0.02);
+  rotateX(frameCount * 0.02);
+  specularMaterial(250);
+  box(100, 100, 100);
+  pop();
+
+  translate(width / 4, 0, 0);
+  ambientMaterial(250);
+  sphere(120, 64);
+}
+
+function drawNewScene() {
+  let t = frameCount / 60; // update time
+  // create a random number of snowflakes each frame
+  for (let i = 0; i < random(5); i++) {
+    snowflakes.push(new snowflake()); // append snowflake object
+  }
+  // loop through snowflakes with a for..of loop
+  for (let flake of snowflakes) {
+    flake.update(t); // update snowflake position
+    flake.display(); // draw snowflake
+  }
+}
+// snowflake class
+function snowflake() {
+  // initialize coordinates
+  this.posX = 0;
+  this.posY = random(-50, 0);
+  this.initialangle = random(0, 2 * PI);
+  this.size = random(2, 5);
+  // radius of snowflake spiral
+  // chosen so the snowflakes are uniformly spread out in area
+  this.radius = sqrt(random(pow(width / 2, 2)));
+  this.update = function(time) {
+    // x position follows a circle
+    let w = 0.6; // angular speed
+    let angle = w * time + this.initialangle;
+    this.posX = width / 2 + this.radius * sin(angle);
+    // different size snowflakes fall at slightly different y speeds
+    this.posY += pow(this.size, 0.5);
+    // delete snowflake if past end of screen
+    if (this.posY > height) {
+      let index = snowflakes.indexOf(this);
+      snowflakes.splice(index, 1);
+    }
+  }
+  this.display = function() {
+    ellipse(this.posX, this.posY, this.size);
+  };
+}
+}
+  this.display = function() {
+    ellipse(this.posX, this.posY, this.size);
+  };
