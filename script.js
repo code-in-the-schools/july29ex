613aef045e17978b3ec6d4edb1209b42e6be1fea
TyTy <Tylieafter1@gmail.com>
2020-07-29 17:24:13 +0000

    we made a clock

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-
-  function setup() {
-  createCanvas(710, 400, WEBGL);
-  snowflake();
-  stroke(255);
-
 let cx, cy;
 let secondsRadius;
 let minutesRadius;
 let hoursRadius;
 let clockDiameter;
-
+  
 function setup() {
   createCanvas(720, 400);
   stroke(255);
@@ -61,32 +55,6 @@ function draw() {
   endShape();
 }
 
-
-  
- 
-function drawScene() {
-   background('brown');
-
-  let locX = mouseX - height / 2;
-  let locY = mouseY - width / 2;
-
-  ambientLight(50);
-  directionalLight(255, 0, 0, 0.25, 0.25, 0);
-  pointLight(0, 0, 255, locX, locY, 250);
-
-  push();
-  translate(-width / 4, 0, 0);
-  rotateZ(frameCount * 0.02);
-  rotateX(frameCount * 0.02);
-  specularMaterial(250);
-  box(100, 100, 100);
-  pop();
-
-  translate(width / 4, 0, 0);
-  ambientMaterial(250);
-  sphere(120, 64);
-}
-
 function drawNewScene() {
   let t = frameCount / 60; // update time
   // create a random number of snowflakes each frame
@@ -126,7 +94,3 @@ function snowflake() {
     ellipse(this.posX, this.posY, this.size);
   };
 }
-}
-  this.display = function() {
-    ellipse(this.posX, this.posY, this.size);
-  };
